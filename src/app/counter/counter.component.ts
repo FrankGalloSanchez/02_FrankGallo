1bfe006bb728a7947a73e4d1e44f2822cec45cb6
Gimena <111780777+FrankGalloSanchez@users.noreply.github.com>
2023-08-30 22:30:46 +0000

    Modificacion del App Contador

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -3,23 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css'],
+  styleUrls: ['./counter.component.css']
 })
 export class CounterComponent {
-  counter = 0;
-  activeDecrease = false;
+  counter = 0;                 // El valor del contador
+  activeDecrease = false;      // Controla si el botón de disminución está activo o no
 
   increase() {
-    // this.counter = this.counter + 1;
-    this.counter++;
+    this.counter++;            // Incrementa el contador
+    this.updateActiveDecrease(); // Actualiza el estado del botón de disminución
   }
 
   decrease() {
-    // this.counter = this.counter - 1;
-    this.counter--;
+    if (this.counter > 0) {     // Solo disminuye si el contador es mayor que 0
+      this.counter--;          // Disminuye el contador
+      this.updateActiveDecrease(); // Actualiza el estado del botón de disminución
+    }
   }
 
   inactiveDecrease() {
-    this.activeDecrease = !this.activeDecrease;
+    this.activeDecrease = !this.activeDecrease; // Activa/desactiva el botón de disminución
+  }
+
+  private updateActiveDecrease() {
+    this.activeDecrease = this.counter === 0; // Actualiza el estado del botón de disminución basado en el contador
   }
 }
